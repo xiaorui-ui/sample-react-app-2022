3ca964de90c0469472935131b9809a84b106ccab
xiaorui-ui <xiaoruizhang9876@gmail.com>
2023-04-08 12:35:25 +0800

    test edit

--- src/pages/DeadEnd.tsx
+++ src/pages/DeadEnd.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const DeadEnd: React.FC = () => {
     return (
         <div style={{ width: '25vw', margin: 'auto', textAlign: 'center' }}>
-            {/* <plaintext>{'text'}</plaintext>, doesn't work beacsue the style is alr specified*/}
-            <h6> {"It's a trap!"} </h6>
+            <p>{"plaintext"}</p>
         </div>
     );
 };
@@ -13,3 +12,4 @@ export default DeadEnd;
 
 // Yay I created my first page!
 // Remember to include in the app, import it(else how else would the app know where it is right?) and include its url
+// <p> to include plaintext
